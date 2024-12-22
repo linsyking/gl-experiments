d724ed46e4d626b706fba3214228e50cff6543d4
linsyking <kxiang@umich.edu>
2024-12-22 10:00:50 -0500

    texture

--- src/app.js
+++ src/app.js
@@ -15,34 +15,36 @@ async function init() {
 
         // console.log(time)
 
-        // let num = 40;
-        // for (let i = 0; i < num * 2; i++) {
-        //     for (let j = 0; j < num * 2; j++) {
-        //         // drawTriangle(-1+i/num+0.02, 1 - j/num-0.03)({
-        //         //     color: [
-        //         //         Math.cos(time * 0.001),
-        //         //         Math.sin(time * 0.0008),
-        //         //         Math.cos(time * 0.003),
-        //         //         1
-        //         //     ],
-        //         //     offset : [0,0]
-        //         // })
-        //         let modd = (time % 10) / 20;
-        //         let xof = -1 + i / num + modd;
-        //         let yof = 1 - j / num;
-        //         drawTriangle({
-        //             x: [xof, yof],
-        //             y: [0.02 + xof, yof],
-        //             z: [0.01 + xof, 0.02 + yof],
-        //             uTime: time + j / num
-        //         })
-        //         // drawRect({
-        //         //     off : [xof,yof],
-        //         //     scale : [0.01, 0.01]
-        //         // })
-        //     }
-        // }
-        drawText()
+        let num = 40;
+        for (let i = 0; i < num * 2; i++) {
+            for (let j = 0; j < num * 2; j++) {
+                // drawTriangle(-1+i/num+0.02, 1 - j/num-0.03)({
+                //     color: [
+                //         Math.cos(time * 0.001),
+                //         Math.sin(time * 0.0008),
+                //         Math.cos(time * 0.003),
+                //         1
+                //     ],
+                //     offset : [0,0]
+                // })
+                let modd = (time % 4) / 2;
+                let xof = -1 + i / num + modd;
+                let yof = 1 - j / num;
+                // drawTriangle({
+                //     x: [xof, yof],
+                //     y: [0.02 + xof, yof],
+                //     z: [0.01 + xof, 0.02 + yof],
+                //     uTime: time + j / num
+                // })
+                drawText({
+                    offset: [xof, yof]
+                })
+                // drawRect({
+                //     off : [xof,yof],
+                //     scale : [0.01, 0.01]
+                // })
+            }
+        }
     })
 }
 
--- src/texture/texture.js
+++ src/texture/texture.js
@@ -1,42 +1,55 @@
 const regl = require('regl')()
 
 const drawTexture = async function () {
-    const image = new Image();
-    image.src = 'enemy.png';
-    await image.decode();
+  const image = new Image();
+  image.src = 'enemy.png';
+  await image.decode();
 
-    return regl({
-        frag: `
+  return regl({
+    frag: `
         precision mediump float;
         uniform sampler2D texture;
         varying vec2 uv;
         void main () {
           gl_FragColor = texture2D(texture, uv);
-        //   gl_FragColor = vec4(1,0,0,1);
         }`,
-    
-        vert: `
+
+    vert: `
         precision mediump float;
         attribute vec2 position;
+        attribute vec2 texc;
+        uniform vec2 offset;
         varying vec2 uv;
         void main () {
-          uv = position *0.5+0.5;
-          gl_Position = vec4((position)*0.1, 0, 1);
+          uv = texc;
+          gl_Position = vec4(-position+offset, 0, 1);
         }`,
-    
-        attributes: {
-            position: [
-                -2, 0,
-                0, -2,
-                2, 2]
-        },
-    
-        uniforms: {
-            texture: regl.texture(image)
-        },
-    
-        count: 3
-    })
-} 
+
+    attributes: {
+      texc: [
+        1, 1,
+        1, 0,
+        0, 0,
+        0, 1,],
+      position: [
+        0.02, 0.02,
+        0.02, -0.02,
+        -0.02, -0.02,
+        -0.02, 0.02,]
+    },
+
+    uniforms: {
+      texture: regl.texture(image),
+      offset: regl.prop('offset')
+    },
+
+    elements: [
+      0, 1, 2,
+      0, 2, 3
+    ],
+
+    count: 6
+  })
+}
 
 export { drawTexture };
